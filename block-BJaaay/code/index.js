579c5974140e2074f505a68e192535c3736d8b2f
FAISAL MAHBOOB <faisal.mahboob2@gmail.com>
2023-05-24 18:54:30 -0400

    completed

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,34 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+//for (var i=1;i<11;i++){
+  //  console.log(i);
+//}
+
 // Using `console.log` log all the evan values from 1 to 10.
+
+//for(var i=2;i<11;i=i+2){
+  //  console.log(i);
+//}
 
 // Using `console.log` log all the od values from 1 to 10.
 
+//for(i=1;i<11;i=i+2){
+  //console.log(i);
+//}
+
 // Calculate the sum of all numbers from 1 to 10.
+//let add=0;
+//for(i=1;i<11;i++){
+  //console.log(i);
+  //add=add+i;
+//}
+//console.log(add);
 
 // Log all the values from 1 to 10 using while loop
+
+let i=0;
+while (i<10){
+  i=i+1;
+  console.log(i);
+}
